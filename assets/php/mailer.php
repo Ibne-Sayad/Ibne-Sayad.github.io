ce3c9dfb391f9911b5e8c7dfe09ed37280be5492
ibnesayad <38115548+ibnesayad@users.noreply.github.com>
2022-02-28 11:30:51 +0600

    mail added

--- assets/php/mailer.php
+++ assets/php/mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "reduan123@gmail.com";
+        $recipient = "ibnsaad8772@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
